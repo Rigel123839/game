ee0f13b4f1d6c836d5595aae4f079b20e514968d
Rigel123839 <Rigelminge@gmail.com>
2024-12-18 10:02:24 +0100

    Add files via upload
    fixed movement bug
    top left?????????????????????????????????

---
+++ game/JavaScript/movement.js
@@ -0,0 +1,98 @@
+var cords = document.getElementById("position");
+const player = document.getElementById("character");
+// movement speed definition
+let ms = 5;
+// start position of player
+let x = 1000;
+let y = 500;
+
+// get player
+const sondreLeft = document.querySelector(".sondreLeft");
+const sondreRight = document.querySelector(".sondreRight");
+
+player.style.position = "absolute";
+player.style.top = y + "px";
+player.style.left = x + "px";
+
+// check player size
+const playerWidth = player.offsetWidth;
+const playerHeight = player.offsetHeight;
+// screen size
+const viewHeight = window.innerHeight;
+const viewWidth = window.innerWidth;
+
+// update player position
+function updatePlayerPos(x, y) {
+  // check player not OOB
+  if (x < 0) x = 0; // cant move more left than 0
+  if (x + playerWidth > viewWidth) x = viewWidth - playerWidth; // cant move more right than screen
+  if (y < 0) y = 0; // cant move more up than 0
+  if (y + playerHeight > viewHeight) y = viewHeight - playerHeight; // cant move more down than screen
+
+  // update player position
+  player.style.left = x + "px";
+  player.style.top = y + "px";
+}
+
+// sondre functions to make the model change when walking
+function sondreMain() {
+  /* 
+  sondreMain.classList.remove("unseen"); */
+  sondreRight.classList.add("unseen");
+  sondreLeft.classList.add("unseen");
+}
+function leftSondre() {
+  /* 
+  sondreMain.classList.add("unseen"); */
+  sondreRight.classList.add("unseen");
+  sondreLeft.classList.remove("unseen");
+}
+function rightSondre() {
+  /* 
+  sondreMain.classList.add("unseen"); */
+  sondreRight.classList.remove("unseen");
+  sondreLeft.classList.add("unseen");
+}
+
+// save keys pressed
+const keysPressed = {};
+
+// keydown press handling
+document.addEventListener("keydown", (event) => {
+  // mark key as pressed
+  keysPressed[event.key] = true;
+});
+// keyup press handling
+document.addEventListener("keyup", (event) => {
+  // mark key as pressed
+  keysPressed[event.key] = false;
+});
+
+function movementLoop() {
+  if (keysPressed["w"] && y > 0) {
+    y -= ms;
+    moDrink();
+    updatePlayerPos(x, y);
+  }
+  if (keysPressed["a"] && x > 0) {
+    x -= ms;
+    moDrink();
+    leftSondre();
+    updatePlayerPos(x, y);
+  }
+  if (keysPressed["s"] && y + playerHeight < window.innerHeight) {
+    y += ms;
+    moDrink();
+    updatePlayerPos(x, y);
+  }
+  if (keysPressed["d"] && x + playerWidth < window.innerWidth) {
+    x += ms;
+    moDrink();
+    rightSondre();
+    updatePlayerPos(x, y);
+  }
+requestAnimationFrame(movementLoop);
+}
+
+
+movementLoop();
---
+++ game/JavaScript/randomBurnSpawn.js
@@ -0,0 +1,84 @@
+// area to spawn burn in.
+const area = document.getElementById("area");
+// get burn
+const burn = new Image();
+burn.src = "../media/camera.jpg";
+document.getElementById("burn_container").appendChild(burn);
+burn.style.width = "auto";
+burn.style.height = "100px";
+// get player element
+const player1 = document.getElementById("character");
+
+// make random positions for burn
+let b_x = 0;
+let b_y = 0;
+let score = -1;
+
+// updating score count
+function updateScore() {
+  // get player score
+  const scoreCount = document.getElementById("player_score");
+  // score starts at 0
+
+  score++;
+  scoreCount.innerHTML = "SCORE: " + score;
+  console.log(score);
+}
+// timer for respawn of burn
+let respawntime = true;
+// move burn to random spot after drinking
+function moveBurn() {
+  // update score
+  updateScore();
+
+  if (!respawntime) return;
+
+  // make unable to respawn
+  respawntime = false;
+
+  // remove burn
+  burn.style.display = "none";
+
+  // delay burn to reapear
+  setTimeout(() => {
+    // get areasize
+    const areaSize = area.getBoundingClientRect();
+    const burnSize = 250;
+
+    // random spot
+    b_y = Math.floor(Math.random() * (window.innerHeight - burnSize));
+    b_x = Math.floor(Math.random() * (window.innerWidth - burnSize));
+
+    // make absolute
+    burn.style.position = "absolute";
+    // show burn again
+    burn.style.display = "block";
+    // change position of burn
+    burn.style.top = b_y + "px";
+    burn.style.left = b_x + "px";
+
+    // respawn
+    respawntime = true;
+  }, 1000);
+}
+
+function moDrink() {
+  // let margin of pickup
+  const overunder = 10;
+
+  // get player position and burn
+  let p_coordinates = player1.getBoundingClientRect();
+  let b_coordinates = burn.getBoundingClientRect();
+
+  if (
+    Math.floor(p_coordinates.top - b_coordinates.top) <= overunder &&
+    Math.floor(p_coordinates.left - b_coordinates.left) <= overunder
+  ) {
+    console.log("touching");
+    moveBurn(b_x, b_y);
+  }
+}
+
+moveBurn();
+setInterval(moDrink, 500);
+/* console.log(Math.floor(p_coordinates.top, b_coordinates.top)); */
